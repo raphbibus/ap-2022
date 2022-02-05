38611cbd9d16f1d10a39926aaa56686267266556
raphbibus <github@raphbib.us>
2022-02-05 16:02:05 +0100

    best slogans ever lol rofl

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,8 +1,13 @@
 <x-navigation>
     <div class="min-h-screen bg-gradient-to-r from-punk-pink via-transparent to-transparent basis-full flex items-center justify-end z-10 mr-6">
         <div class="text-right">
-            <h1 class="font-display text-8xl text-punk-pink">agile punks</h1>
-            <p class="font-sans text-5xl">because you're not agile enough</p>
+            <h1 class="font-display text-9xl text-punk-white">agile punks</h1>
+            <p class="font-sans text-3xl">
+                Organisationsingenieure aus Leidenschaft.<br>
+                Antiberater aus Überzeugung.<br>
+                Dieser kleine Lichtblick im Sturm deiner Transformation.
+                <span class="text-5xl text-punk-pink block pt-12">Agil, weil's sonst niemand ist.</span>
+            </p>
         </div>
     </div>
     <video autoplay loop muted class="absolute w-auto min-w-full min-h-full max-w-none hidden md:block">
