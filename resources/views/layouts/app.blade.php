bb9566a94f08f9d29bc754f8b904ca45bd4467ba
raphbibus <github@raphbib.us>
2022-02-25 23:02:21 +0100

    footer is now a component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,19 +17,7 @@
         <div id="page_content" class="min-w-full container">
             {{$slot}}
         </div>
-        <footer class="w-full py-28 xl:py-16">
-            <div class="text-center w-1/2 mx-auto">
-                <span class="text-6xl font-display text-punk-darker hover:text-punk-pink">
-                    <a href="/">agile punks</a>
-                </span>
-                <hr class="border-punk-darker my-2"/>
-                <span class="text-3xl text-punk-darker font-sans">einmal punk, immer punk.<br>est. 2018</span>
-                <hr class="border-punk-darker my-2"/>
-                <span class="text-xl font-extrabold font-serif text-punk-darkred hover:text-punk-pink underline">
-                    <a href="/impressum">impressum und das übliche</a>
-                </span>
-            </div>
-        </footer>
+        <x-footer></x-footer>
         @livewireScripts
     </body>
 </html>
