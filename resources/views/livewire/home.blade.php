0296cac90b09b7c4db92fe1794a31c796225c04a
raphbibus <github@raphbib.us>
2022-02-26 14:26:22 +0100

    text colors at home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,14 +2,14 @@
     <div class="min-h-screen bg-punk-pink p-8
         xl:bg-transparent xl:bg-gradient-to-r xl:from-punk-pink xl:via-transparent xl:to-transparent basis-full flex items-center justify-end xl:z-10 xl:mr-6 xl:p-0">
         <div class="text-right">
-            <h1 class="hidden xl:block font-display text-9xl text-punk-white">agile punks</h1>
-            <p class="font-sans text-3xl">
+            <h1 class="hidden xl:block font-display text-9xl text-punk-light">agile punks</h1>
+            <p class="font-sans text-xl text-punk-dark xl:text-punk-light">
                 Organisationsingenieure aus Leidenschaft.<br>
                 Antiberater aus Überzeugung.<br>
                 Dieser kleine Lichtblick im Sturm deiner Transformation.
-                <span class="text-5xl xl:text-punk-pink block pt-12">Agil, weil's sonst niemand ist.</span>
+                <span class="text-3xl text-punk-dark xl:text-punk-pink block pt-12">Agil, weil's sonst niemand ist.</span>
             </p>
-            <h1 class="xl:hidden font-display text-7xl text-punk-white mt-8">agile punks</h1>
+            <h1 class="xl:hidden font-display text-7xl text-punk-dark mt-8">agile punks</h1>
         </div>
     </div>
     <video autoplay loop muted class="absolute w-auto min-w-full min-h-full max-w-none hidden xl:block">
