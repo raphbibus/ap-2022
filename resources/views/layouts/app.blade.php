65f550fd36dcca372e28a7630c046c0e54a25bb1
raphbibus <github@raphbib.us>
2022-03-14 21:02:54 +0100

    added asset versioning for css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
         <x-fonts></x-fonts>
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
         @livewireStyles
     </head>
